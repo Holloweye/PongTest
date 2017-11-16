3565540a5c2eeeada4ad927cc9aab9a6234aa211
Christer Ulfsparre Lyremar <christer@dynamo.mobi>
2017-11-16 06:51:44 +0100

    Added motion trail to ball.

---
+++ Assets/Scripts/Animations/FadeAway.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeAway : MonoBehaviour 
+{
+	public float life;
+	private float counter = 0f;
+	private SpriteRenderer rendering;
+
+	void Start()
+	{
+		this.rendering = GetComponent<SpriteRenderer> ();
+	}
+
+	void Update () 
+	{
+		this.counter += Time.deltaTime;
+
+		if (this.counter >= this.life) {
+			Destroy (this.gameObject);
+		} else {
+			this.rendering.color = new Vector4(
+				this.rendering.color.r,
+				this.rendering.color.g,
+				this.rendering.color.b,
+				1f - this.counter / this.life
+			);
+		}
+	}
+}
---
+++ Assets/Scripts/Animations/Spawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+	public string spawn;
+	public float spawnRate;
+	private float counter = 0f;
+	private GameObject toSpawn;
+	private SpriteRenderer rendering;
+
+	void Start()
+	{
+		this.toSpawn = (GameObject)Resources.Load (this.spawn);
+		this.rendering = GetComponent<SpriteRenderer> ();
+	}
+
+	void Update () 
+	{
+		this.counter += Time.deltaTime;
+		if (this.counter >= this.spawnRate)
+		{
+			this.counter = 0f;
+			var obj = Instantiate (this.toSpawn);
+			obj.transform.position = this.transform.position;
+			obj.transform.rotation = this.transform.rotation;
+
+			var objRendering = obj.GetComponent<SpriteRenderer> ();
+			objRendering.color = this.rendering.color;
+		}
+	}
+}
